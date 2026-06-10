269688c094a1fb91cd2b57017e9a1330b17809f6
Darwish <289582304@qq.com>
2026-06-10 08:07:17 +0800

    test: round test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use lightningcss::stylesheet::{ParserOptions, PrinterOptions, StyleSheet};
+
 fn main() {
-    println!("Hello, world!");
+    let css = ".used { color: red } .unused { color: blue }";
+
+    let sheet = StyleSheet::parse(css, ParserOptions::default()).unwrap();
+    let out = sheet.to_css(PrinterOptions::default()).unwrap();
+
+    println!("{}", out.code);
 }
